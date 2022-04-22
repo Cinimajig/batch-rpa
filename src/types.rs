b50f899264b96409dfa92d92163153038682b864
Cinimajig <jacob.mogensen5@gmail.com>
2022-04-22 12:25:14 +0200

    FromStr for NumberValue

--- src/types.rs
+++ src/types.rs
@@ -30,6 +30,18 @@ impl FromStr for NumberValue {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        if let Ok(float) = s.parse::<f64>() {
+            return Ok(NumberValue::Float(float));
+        }
+        if let Ok(int_t) = s.parse::<usize>() {
+            return Ok(NumberValue::Unsigned(int_t));
+        }
+        let result = s.parse::<isize>();
+
+        if result.is_err() {
+            return Err(result.unwrap_err());
+        }
+        
+        Ok(NumberValue::Signed(result.unwrap()))
     }
 }
