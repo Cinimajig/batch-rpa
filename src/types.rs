a03a58f6455fe137a867748cf1387b31c6e83558
Cinimajig <39052490+Cinimajig@users.noreply.github.com>
2022-04-22 11:49:24 +0200

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -5,13 +5,28 @@ pub enum Type {
     None,
     String(String),
     Char(char),
-    Number(isize),
+    Number(NumberValue),
     Bool(bool),
     List(Vec<Type>),
     Object(HashMap<String, Type>),
 }
 
+#[derive(Debug)]
+pub enum NumberValue {
+    Float(f64),
+    Signed(isize),
+    Unsigned(usize),
+}
+
 impl FromStr for Type {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+impl FromStr for NumberValue {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
